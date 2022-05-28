d94aecfd31bc85032192774f4fe29b6aeb21c7de
Andres Toller <andres.mza92@gmail.com>
2022-05-28 07:16:11 -0300

    bugfixes

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
 console.log("hola mundo");
-
-var percentageComplete = 0.9;
-var strokeDashOffsetValue = 100 - (percentageComplete * 100);
-var progressBar = $(".js-progress-bar");
-progressBar.css("stroke-dashoffset", strokeDashOffsetValue);
